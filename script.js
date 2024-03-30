a49f10306b09b47e51c79da402c962accc8321a5
jasur <jasur@gmial.com>
2024-03-31 04:05:04 +0500

    hello

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function Open(){
+    document.getElementById("menu-box").style.display = "block";
+}
+function Close(){
+    document.getElementById("menu-box").style.display = "none";
+}
+function SearchBtn(){
+    document.getElementById("Searching-engine").style.display = "block";
+}
+function SearchClose(){
+    document.getElementById("Searching-engine").style.display = "none";
+}
